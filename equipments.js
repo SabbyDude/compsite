d42a860db8993ea65b043d3d84ed15acea723c44
SabbyDoof <75109982+SabbyDude@users.noreply.github.com>
2026-08-28 21:26:34 +0530

    Update equipments.js

--- equipments.js
+++ equipments.js
@@ -2,7 +2,7 @@ const PERIPHERAL_DATA = {
     monitor: {
         name: "Epsilon Display",
         sizes: { "70cm": 50000, "85cm": 100000, "100cm": 200000 },
-        resolutions: { "2500x2000": 50000, "3750x3000": 100000, "5000x4000": 200000 },
+        resolutions: { "3000x2000": 50000, "4500x3000": 100000, "6000x4000": 200000 },
         refreshRates: { "100Hz": 50000, "200Hz": 100000, "300Hz": 200000 }
     },
     keyboard: {
@@ -20,6 +20,6 @@ const PERIPHERAL_DATA = {
     },
     mic: {
         name: "Epsilon Speak",
-        types: { "Dynamic": 8000, "Condenser": 14000, "Ribbon": 20000 }
+        types: { "Dynamic": 10000, "Condenser": 20000, "Ribbon": 30000 }
     }
 };
